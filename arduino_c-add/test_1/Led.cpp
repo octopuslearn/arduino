1095f7f804dedb3170548f9bdeef962acd9ba4fd
octopuslearn <3148262242@qq.com>
2023-07-19 17:24:46 +0800

    arduino .cpp .h文件
    .h中存放类声明
    .cpp存放
    
    #ifndef __LED_H_//头文件可存放类的声明 //此语句的作用是防止重复定义
    #define __LED_H_
    
    #endif

---
+++ arduino_c-add/test_1/Led.cpp
@@ -0,0 +1,41 @@
+#include "Led.h"
+
+//为构造函数里的变量赋值
+//Led::Led(int userLedPin){//参数必须和Led(int userLedPin);//构造函数-有参数一致
+//  Serial.println("Led::Led(int userLedPin)---succeed");
+//  ledPin = userLedPin;  
+//  pinMode(userLedPin,OUTPUT);
+//}
+/*以下，等同上*/
+//为构造函数里的变量赋值
+Led::Led(int userLedPin) : ledPin(userLedPin){//参数必须和Led(int userLedPin);//构造函数-有参数一致
+  Serial.println("Led::Led(int userLedPin)---succeed");
+  pinMode(userLedPin,OUTPUT);
+}
+/*以上，等同上*/
+
+Led::Led(){//Led()此函数来自Led类
+  Serial.println("ssss");
+  pinMode(2,OUTPUT);
+}
+Led::~Led(){
+  Serial.println("aaa");
+}
+
+void Led::off(){
+  digitalWrite(ledPin,LOW);
+}
+
+void Led::on(){//on()属于Led类
+  digitalWrite(ledPin,HIGH);
+}
+
+int Led::getledPin(){
+  return ledPin;
+}
+
+void Led::change_ledPin(int userLedPin){
+  ledPin = userLedPin;
+  pinMode(ledPin,OUTPUT);
+  Serial.println("void Led::change_ledPin(int userLedPin)---更改完成");
+}
--- arduino_c-add/test_1/test_1.ino
+++ arduino_c-add/test_1/test_1.ino
@@ -1,66 +1,6 @@
 /*弹幕说，构造函数名称和类名称相同*/
 
-class Led{//建立类
-  public://在pulic后的是公共成员函数（变量），公共成员函数(变量)可以在类的外面调用
-  /*//构造函数-无返回值*/ //注意：构造函数不可在类的外面调用
-  //非必须的
-    Led();//构造函数  ////执行完这个//Led myLed;建立Led类的对象 后马上执行Led();//构造函数
-  /*//析构函数-无返回值*/  //注意：析构函数不可在类的外面调用
-  //非必须的
-    ~Led();//析构函数 是构造函数前加~  
-
-    Led(int userLedPin);//构造函数-有参数
-
-
-    void on();//类的成员函数，公共成员函数可以在类的外面调用
-    void off();//类的成员函数，公共成员函数可以在类的外面调用
-//    int ledPin=2;//公共成员变量
-    int getledPin();//测试通过公共成员函数可以在将私有成员变量在类外使用
-    void change_ledPin(int userLedPin);
-
-   private://类外部无法使用但是，类的内部可以使用
-    int ledPin=2;//私有成员变量
-};
-
-//为构造函数里的变量赋值
-//Led::Led(int userLedPin){//参数必须和Led(int userLedPin);//构造函数-有参数一致
-//  Serial.println("Led::Led(int userLedPin)---succeed");
-//  ledPin = userLedPin;  
-//  pinMode(userLedPin,OUTPUT);
-//}
-/*以下，等同上*/
-//为构造函数里的变量赋值
-Led::Led(int userLedPin) : ledPin(userLedPin){//参数必须和Led(int userLedPin);//构造函数-有参数一致
-  Serial.println("Led::Led(int userLedPin)---succeed");
-  pinMode(userLedPin,OUTPUT);
-}
-/*以上，等同上*/
-
-Led::Led(){//Led()此函数来自Led类
-  Serial.println("ssss");
-  pinMode(2,OUTPUT);
-}
-Led::~Led(){
-  Serial.println("aaa");
-}
-
-void Led::off(){
-  digitalWrite(ledPin,LOW);
-}
-
-void Led::on(){//on()属于Led类
-  digitalWrite(ledPin,HIGH);
-}
-
-int Led::getledPin(){
-  return ledPin;
-}
-
-void Led::change_ledPin(int userLedPin){
-  ledPin = userLedPin;
-  pinMode(ledPin,OUTPUT);
-  Serial.println("void Led::change_ledPin(int userLedPin)---更改完成");
-}
+#include "Led.h"
 
 void setup() {
   Serial.begin(9600);
