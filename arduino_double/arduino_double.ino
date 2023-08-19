b3e1336b2372319999ad8ddc1fecc60ab0e24212
octopuslearn <3148262242@qq.com>
2023-08-20 07:16:44 +0800

    iic主设备-待验证（缺少arduino)

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,46 +1,34 @@
-#include "Wire.h"//iic,twi(twi兼容iic，是为了逃避iic专利费的)
+#include <Wire.h>
 
 #define ADDRESS 0x8
-byte temp = 24;//摄氏度
-byte unitType = 0;//0-摄氏度，1-华氏度
-
-
-void requestEvent();
-void receiveEvent(int howMany_bytes);
+#define TEMP_TYPE 1 //1-华氏度，0-摄氏度
 
 void setup()
 {
   Serial.begin(9600);
-  Wire.begin(ADDRESS);
-  Wire.onReceive(receiveEvent);//收到数据后，执行receiveEvent。从设备接收便调用。设定到底是摄氏度还是华氏度
-  Wire.onRequest(requestEvent);//收到需求指令，执行requestEvent。从设备相应请求。获取当前的温度值
+  Wire.begin();
+
+  Wire.beginTransmission(ADDRESS);
+  Wire.write("UNIT");
+  Wire.write(TEMP_TYPE);//华氏度
+  Wire.endTransmission();
 }
 
 void loop()
 {
-  delay(100);
-}
-
-//收到数据时执行，即从机接收到数据时调用此函数
-void receiveEvent(int howMany_bytes)
-{
-  String cmd = " ";//UNIT0-5 bytes
-  while(Wire.available() > 1)//读取UNIT，而不读取0/1
+  delay(1000);
+  Wire.requestFrom(ADDRESS, 1);//向ADDRESS地址的从设备请求一个字节
+  while(Wire.available())
   {
-    char c = Wire.read();
-    cmd.concat(c);
-  }
-  Serial.println(cmd);//读取0/1
-  if(cmd == "UNIT") unitType = Wire.read();
-  cmd = " ";
-}
+    byte temp = Wire.read();
 
-void requestEvent()
-{
-  switch(unitType)
-  {
-    case 0:Wire.write(temp);break;//返回摄氏度
-    case 1:Wire.write((uint8_t)((temp*1.8)+32));break;//返回华氏度
-    default:break;
+    Serial.print("Temperature = ");
+    Serial.print(temp);
+    switch(TEMP_TYPE)
+    {
+      case 0:Serial.println("C");break;
+      case 1:Serial.println("F");break;
+      default:break;
+    }
   }
 }
