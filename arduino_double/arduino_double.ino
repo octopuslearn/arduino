aa5be189d83dca5d098bd115eb8d4159871143d8
octopuslearn <3148262242@qq.com>
2023-08-19 13:48:21 +0800

    onebutton库
    貌似最多只能三击
    void btnattachMultiClick()
    {
      static int button_count = btn1.getNumberClicks();//获取点击次数
      switch(button_count)
      {
        // case 3:Serial.println("3击");break;
        default:Serial.print("点击次数： ");Serial.println(button_count);break;
      }
    }

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,106 +1,66 @@
+#include "OneButton.h"
 
-#define  button 2
-#define  debounce 120//消抖
+#define button1 2
+
+//按下GND有效
+OneButton btn1 = OneButton(
+  button1,  // Input pin for the button
+  true,        // Button is active LOW
+  true         // Enable internal pull-up resistor
+);
 
 
-bool btnStatusPrevious = 0;
-bool btnStatusCurrent = 0;
-
-
-
-int i = 0; 
 void setup() {
-
   Serial.begin(9600);
-  pinMode(button, INPUT);
+
+  btn1.setDebounceMs(10);//消抖
+  btn1.setClickMs(1500);//用于区分单击和多击，即500ms内记录按下几次
+  btn1.setPressMs(3000);//长按时间判定，800ms后算长按
+  
+  
+  btn1.attachClick(btnattachClick);//单击
+  btn1.attachDoubleClick(btnattachDoubleClick);//双击
+  btn1.attachMultiClick(btnattachMultiClick);//多击
+  btn1.attachLongPressStart(btnattachLongPressStart);//按住按键800ms后触发
+  // btn1.attachDuringLongPress(btnattachDuringLongPress);//按住按钮定期触发
+  btn1.attachLongPressStop(btnattachLongPressStop);//长时间按住按钮松开时触发
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  btnStatusCurrent = digitalRead(button);
-
-
-// //按键被一直按下去
-//   if(btnStatusCurrent == HIGH)
-//   {
-//     Serial.println("按键按下");
-//   }
-//   else
-//   {
-//     Serial.println("按键弹起");
-//   }
-  
-  // //按键按下去的瞬间
-  // //当前按钮状态是high，之前按键状态是low--->按下
-  // if((btnStatusCurrent == HIGH) && (btnStatusPrevious == LOW))
-  // {
-  //   i++;//按一下累计了七次
-  //   Serial.print("i: ");Serial.print(i);Serial.print("\t");
-  //   Serial.println("按键按一下");
-  //   // delay(debounce);//消抖？？？--->没错消抖了
-  //   i = 0;
-  // }
-
-  // if((btnStatusCurrent == LOW) && (btnStatusPrevious == HIGH))
-  // {
-  //   i++;//按一下累计了七次
-  //   Serial.print("i: ");Serial.print(i);Serial.print("\t");
-  //   Serial.println("弹起");
-  //   // delay(debounce);//消抖？？？--->没错消抖了
-  //   i = 0;
-  // }
-  
-
-//单击，双击，三击
-clickTick();
-
-
-  btnStatusPrevious = btnStatusCurrent;//按键更新
+  btn1.tick();
+  delay(10);
+  // put your main code here, to run repeatedly:
 }
 
-void clickTick()
+
+void btnattachClick()
 {
-  static unsigned int clickMaxperiod = 500; //检测周期
-  static unsigned int preeCount = 0;        //按键按下次数
-  static bool isStart = 0;                  //检测周期开始标志
-  static unsigned long startTime = 0;       //检测周期开始时间
-
-  if((btnStatusCurrent == LOW) && (btnStatusPrevious == HIGH))
+  Serial.println("单击");
+}
+void btnattachDoubleClick()
+{
+  Serial.println("双击");
+}
+void btnattachMultiClick()
+{
+  static int button_count = btn1.getNumberClicks();//获取点击次数
+  switch(button_count)
   {
-    if(isStart == 0)
-    {
-      isStart = 1;//检测周期开始
-      preeCount = 1;//当前按钮次数1
-      startTime = millis();//获取周期开始时间
-    }
-    else
-    {
-      preeCount++;//按键次数累计
-    }
-    delay(debounce);//按键消抖
-  }  
-    
-    if(((millis() - startTime) > clickMaxperiod) && isStart)//检测周期内获取按键按下次数
-    {
-      isStart = 0;//停止检测周期
-      switch(preeCount)
-      {
-        case 1:btnSingle();break;
-        case 2:btnDouble();break;
-        case 3:btnTriple();break;
-        default:break;
-      }
-    }
+    // case 3:Serial.println("3击");break;
+    default:Serial.print("点击次数： ");Serial.println(button_count);break;
+  }
 }
-
-
-void btnSingle(){
-  Serial.println("btnSingle");
+void btnattachLongPressStart()
+{
+  Serial.println("按住按键800ms后触发");
 }
-void btnDouble(){
-  Serial.println("btnDouble");
+void btnattachDuringLongPress()
+{
+  Serial.println("按住按钮定期触发");
 }
-void btnTriple(){
-  Serial.println("btnTriple");
+void btnattachLongPressStop()
+{
+  Serial.println("长时间按住按钮松开时触发");
 }
