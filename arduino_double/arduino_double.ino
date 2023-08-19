b1ddc49c5216cb241b55ac53adf8d514bf70ee96
octopuslearn <3148262242@qq.com>
2023-08-20 07:03:28 +0800

    IIC从设备-待验证（缺少arduino)
    先用扫描程序获取从设备IIC地址
    #include <Wire.h>
    void setup()
    {
      Serial.begin(9600);
      Wire.begin();//无形参，即主机
      Wire.setClock(100000);//默认100k
      Wire.setWireTimeout(300, true);//超时时间???
    }
    void loop()
    {
      Serial.println("Scanning...");
      for(int i = 0;i<128;i++)
      {
        Wire.beginTransmission(i);//准备向指定的地址开始传输数据
        byte code = Wire.endTransmission();//结束传输
        if(code==0)//反馈0，代表受到回应，即应答位Aick=0
        {
          Serial.print("Found 0x");
          Serial.println(i, HEX);
        }
      }
    }

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,25 +1,46 @@
-#include <Wire.h>
+#include "Wire.h"//iic,twi(twi兼容iic，是为了逃避iic专利费的)
+
+#define ADDRESS 0x8
+byte temp = 24;//摄氏度
+byte unitType = 0;//0-摄氏度，1-华氏度
+
+
+void requestEvent();
+void receiveEvent(int howMany_bytes);
 
 void setup()
 {
   Serial.begin(9600);
-  while(!Serial);
-
-  Wire.begin();//无形参，即主机
-  Wire.setClock(100000);//默认100k
-  Wire.setWireTimeout(300, true);//超时时间???
+  Wire.begin(ADDRESS);
+  Wire.onReceive(receiveEvent);//收到数据后，执行receiveEvent。从设备接收便调用。设定到底是摄氏度还是华氏度
+  Wire.onRequest(requestEvent);//收到需求指令，执行requestEvent。从设备相应请求。获取当前的温度值
 }
+
 void loop()
 {
-  Serial.println("Scanning...");
-  for(int i = 0;i<128;i++)
+  delay(100);
+}
+
+//收到数据时执行，即从机接收到数据时调用此函数
+void receiveEvent(int howMany_bytes)
+{
+  String cmd = " ";//UNIT0-5 bytes
+  while(Wire.available() > 1)//读取UNIT，而不读取0/1
   {
-    Wire.beginTransmission(i);//准备向指定的地址开始传输数据
-    byte code = Wire.endTransmission();//结束传输
-    if(code==0)//反馈0，代表受到回应，即应答位Aick=0
-    {
-      Serial.print("Found: 0x");
-      Serial.println(i, HEX);
-    }
+    char c = Wire.read();
+    cmd.concat(c);
+  }
+  Serial.println(cmd);//读取0/1
+  if(cmd == "UNIT") unitType = Wire.read();
+  cmd = " ";
+}
+
+void requestEvent()
+{
+  switch(unitType)
+  {
+    case 0:Wire.write(temp);break;//返回摄氏度
+    case 1:Wire.write((uint8_t)((temp*1.8)+32));break;//返回华氏度
+    default:break;
   }
 }
