be4ee29400786227ef031476393bd7c714a940ff
octopuslearn <3148262242@qq.com>
2023-08-20 09:44:33 +0800

    获取EEPROM大小

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,34 +1,21 @@
-#include <Wire.h>
-
-#define ADDRESS 0x8
-#define TEMP_TYPE 1 //1-华氏度，0-摄氏度
+#include <EEPROM.h>
 
 void setup()
 {
   Serial.begin(9600);
-  Wire.begin();
 
-  Wire.beginTransmission(ADDRESS);
-  Wire.write("UNIT");
-  Wire.write(TEMP_TYPE);//华氏度
-  Wire.endTransmission();
+  unsigned int eepromLength = getEEPROMLength(true);//获取EEPROM大小
 }
-
 void loop()
-{
-  delay(1000);
-  Wire.requestFrom(ADDRESS, 1);//向ADDRESS地址的从设备请求一个字节
-  while(Wire.available())
-  {
-    byte temp = Wire.read();
+{}
 
-    Serial.print("Temperature = ");
-    Serial.print(temp);
-    switch(TEMP_TYPE)
-    {
-      case 0:Serial.println("C");break;
-      case 1:Serial.println("F");break;
-      default:break;
-    }
+
+unsigned int getEEPROMLength(bool choose)
+{
+  unsigned int length = EEPROM.length();//获取EEPROM存储单元
+  if(choose == true)
+  {
+    Serial.print("EEPROM length:  ");Serial.println(length);
   }
+  return length;
 }
