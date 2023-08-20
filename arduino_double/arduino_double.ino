fee22f9f7fe2acabf4d387f3eed3c59cbbeb7699
octopuslearn <3148262242@qq.com>
2023-08-20 19:59:23 +0800

    volatile挥发性变量-2
    volatile int a[200] = {0};//200*2 = 400 bytes//volatile告诉编译器，无论有没有使用都会声明

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,4 +1,4 @@
-int a[200] = {0};//200*2 = 400 bytes
+volatile int a[200] = {0};//200*2 = 400 bytes//volatile告诉编译器，无论有没有使用都会声明
 void setup()
 {
   Serial.begin(9600);
@@ -6,7 +6,7 @@ void setup()
 
   //在没有for这条语句前RAM-188,有了for这条语句后RAM-588
   //这是因为编译器看到变量a声明，但是没有使用（即没有for）时将变量a给删除了
-  for(int i : a) Serial.println(i);//RAM-588(188+400)
+  //for(int i : a) Serial.println(i);//RAM-588(188+400)
 }
 
 void loop()
