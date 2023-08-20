04922d054b8efc46fb007dc6a9bed5d89cb3d341
octopuslearn <3148262242@qq.com>
2023-08-20 09:52:47 +0800

    //获取EEPROM的地址，若果新的uno全是255

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,5 +1,10 @@
 #include <EEPROM.h>
 
+unsigned int length = 0;
+
+void showAll(unsigned int eepromLength);
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -7,15 +12,30 @@ void setup()
   unsigned int eepromLength = getEEPROMLength(true);//获取EEPROM大小
 }
 void loop()
-{}
+{
+  showAll(length);
+}
 
 
 unsigned int getEEPROMLength(bool choose)
 {
-  unsigned int length = EEPROM.length();//获取EEPROM存储单元
+  // unsigned int length = EEPROM.length();//获取EEPROM存储单元
+  length = EEPROM.length();//获取EEPROM存储单元
   if(choose == true)
   {
     Serial.print("EEPROM length:  ");Serial.println(length);
   }
   return length;
+}
+
+
+void showAll(unsigned int eepromLength)
+{
+  for(int add = 0;add < eepromLength; add++){
+    byte data = EEPROM.read(add);
+    Serial.print("Address ");
+    Serial.print(add);
+    Serial.print(" -> ");
+    Serial.println(data);
+  }
 }
