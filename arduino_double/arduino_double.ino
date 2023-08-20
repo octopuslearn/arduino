bb717d5710b0ad6ea28f439f2a49cce3345e2fb7
octopuslearn <3148262242@qq.com>
2023-08-21 06:08:47 +0800

    向量-1

--- arduino_double/arduino_double.ino
+++ arduino_double/arduino_double.ino
@@ -1,15 +1,13 @@
-volatile int a[200] = {0};//200*2 = 400 bytes//volatile告诉编译器，无论有没有使用都会声明
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("");//RAM-188
+#include <Arduino_AVRSTL.h>
 
-  //在没有for这条语句前RAM-188,有了for这条语句后RAM-588
-  //这是因为编译器看到变量a声明，但是没有使用（即没有for）时将变量a给删除了
-  //for(int i : a) Serial.println(i);//RAM-588(188+400)
-}
+using namespace std;
+
+void setup()
+{  
+  Serial.begin(9600);
+  while(!Serial) {};//等待用户打开串口监视器
+} 
 
 void loop()
 {
-
 }
